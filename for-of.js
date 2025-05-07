e8ff6cc60f7f84c6754ca9df37a0f5b22a3e9016
ajim khan <ajkhanbd24@gmail.com>
2025-05-07 19:43:47 +0600

    even

---
+++ for-of.js
@@ -0,0 +1,12 @@
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['aam', 'jam', 'kathal', 'lichu', 'bedena'];
+for (const fruit of fruits) {
+    console.log(fruit)
+    console.log('i want to eat fruits')
+}
